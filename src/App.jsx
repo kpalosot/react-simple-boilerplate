9ce373588d8683561ca453df1b3525228d6dc0bc
Kyla Palos <kyla.palos@gmail.com>
2018-09-04 16:19:46 +0000

    finished day2 tasks

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,65 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      messages: messages
+      messages: messages,
+      currentUser: 'Anonymous'
     }
   }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = { username: "Michelle", content: "Hello there!", type:"incomingMessage" };
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messages: messages })
+    }, 3000);
+  }
+
+  _addIncomingMessage = message => {
+    const username = this.state.currentUser;
+    const newMessage = {
+      content: message,
+      type: 'incomingMessage',
+      username
+    };
+    const allOldMessages = this.state.messages;
+    const allNewMessages = [...allOldMessages, newMessage];
+    this.setState({
+      messages: allNewMessages
+    });
+  }
+
+  // _addNotificationMessage = () => {
+
+  // }
+
+  _updateUsername = username => {
+    const newContent = `${this.state.currentUser} has changed their username to ${username}`;
+    const newMessage = {
+      content: newContent,
+      type: 'incomingNotification'
+    };
+    const allOldMessages = this.state.messages;
+    const allNewMessages = [...allOldMessages, newMessage];
+    this.setState({
+      currentUser: username,
+      messages: allNewMessages
+    });
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <MessageList messages={this.state.messages}/>
-        <ChatBar />
+        <ChatBar
+          currentUser={this.state.currentUser}
+          addMessage={this._addIncomingMessage}
+          updateUsername={this._updateUsername}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 
 export default class ChatBar extends Component {
+  _handleKeyPress = (e) => {
+      if (e.keyCode === 13) {
+        if(e.target.name === 'content'){
+          this.props.addMessage(e.target.value);
+          e.target.value = '';
+        } else {
+          this.props.updateUsername(e.target.value);
+        }
+      }
+  }
+
   render(){
+    const username = this.props.currentUser ? this.props.currentUser : "Anonymous2.0";
     return (
     <footer className="chatbar">
-      <input className="chatbar-username" name="username"placeholder="Your Name (Optional)" />
-      <input className="chatbar-message" name="content" placeholder="Type a message and hit ENTER" />
+      <input
+        className="chatbar-username"
+        name="username"
+        placeholder="Your username (optional)"
+        defaultValue={username}
+        onKeyDown={this._handleKeyPress}/>
+      <input
+        className="chatbar-message"
+        name="content"
+        placeholder="Type a message and hit ENTER"
+        onKeyDown={this._handleKeyPress}/>
     </footer>);
   }
 }
